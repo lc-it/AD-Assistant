ab2f5f3f0ead3d085a039f420e0378c16b9ac116
lc-it <lc-it@locus-cell.com>
2025-09-22 15:20:46 +0800

    修正UI介面及上傳檔案TIME OUT 時間

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 # --- 全域設定 ---
-CURRENT_VERSION = "1.0.7"
+CURRENT_VERSION = "1.0.8"
 UPDATE_INFO_URL = "https://raw.githubusercontent.com/lc-it/AD-Assistant/main/version.json"
 APP_NAME = "MyAssistant.exe"
 
